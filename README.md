b5ee030939129f66252bfb8f46374391c15b13e4
tanaysh7 <tanaysh7@gmail.com>
2018-01-17 05:39:35 +0530

    Added Library and main file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java ML
+This is a starter ML kit in Java makes use of ML library available for Java.
+#### Java Machine Learning Library 0.1.7
+#### API documentation -http://java-ml.sourceforge.net/api/0.1.7
---
+++ src/MLapp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tanay Shankar on 17-Jan-2018
+ *
+ */
+public class MLapp {
+
+	/**
+	 * @param args Reference http://java-ml.sourceforge.net/content/creating-instance
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
